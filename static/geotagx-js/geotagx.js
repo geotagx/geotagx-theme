9a24f5d7b748891d6b3b3bb3cad4f4593a2570f7
S.P. Mohanty <spmohanty91@gmail.com>
2015-03-28 10:38:55 +0530

    Front page project grid **almost** implemented

---
+++ static/geotagx-js/geotagx.js
@@ -0,0 +1,47 @@
+//Fronpage Grid
+$( document ).ready(function() {
+    $(function () {
+
+        var filterList = {
+
+            init: function () {
+
+                // MixItUp plugin
+                // http://mixitup.io
+                $('#portfoliolist-three').mixitup({
+                    targetSelector: '.portfolio',
+                    filterSelector: '.filter',
+                    effects: ['fade'],
+                    easing: 'snap',
+                    // call the hover effect
+                    onMixEnd: filterList.hoverEffect()
+                });
+            },
+            hoverEffect: function () {
+                $("[rel='tooltip']").tooltip();
+                // Simple parallax effect
+                $('#portfoliolist-three .portfolio .portfolio-hover').hover(
+        function(){
+            $(this).find('.image-caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.image-caption').slideUp(250); //.fadeOut(205)
+        }
+    );
+            }
+
+        };
+
+        // Run the show!
+        filterList.init();
+    });
+
+
+    $('.magnefig').each(function(){
+        $(this).magnificPopup({
+            type:'image',
+            removalDelay: 300,
+            mainClass: 'mfp-fade'
+        })
+    });
+});
