e13f30b09828309753989ff7c4252f36076106af
S.P. Mohanty <spmohanty91@gmail.com>
2015-07-18 23:14:42 +0200

    Refs #68 - Adds a central place on the site where all the blog posts can be accessed

--- pybossa_custom_filters.py
+++ pybossa_custom_filters.py
@@ -4,3 +4,20 @@
 def geotagx_md5(s):
 	import hashlib
 	return hashlib.md5(s).hexdigest()
+
+def geotagx_blog_image(body, default_image):
+	# Note : The body is supposed to be in MarkDown
+	import re
+	links = re.findall('!\[.*\]\((.*)\)', body)
+	if len(links) == 0:
+		return default_image
+	else:
+		# Return the first image link if there are indeed multiple images in the body
+		return links[0]
+
+def geotagx_blog_trim_body(body):
+	CHARACTERS_IN_SHORT_DESCRIPTION = 400
+	if len(body) < CHARACTERS_IN_SHORT_DESCRIPTION :
+		return body
+	else:
+		return body[:CHARACTERS_IN_SHORT_DESCRIPTION]+"..."
