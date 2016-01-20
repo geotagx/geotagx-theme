173765308b1c39a102ffec3bc9b8dbd9d504b690
Jeremy Othieno <j.othieno@gmail.com>
2016-01-20 21:29:05 +0100

    Add project-specific analytics
    References geotagx/geotagx-project-theme@d3c1033.

--- static/geotagx-js/geotagx-analytics.js
+++ static/geotagx-js/geotagx-analytics.js
@@ -1,8 +1,15 @@
 /*
  * GeoTag-X page analytics.
  */
-;(function($, undefined){
+;(function(geotagx, $, undefined){
 	"use strict";
+
+	geotagx.analytics = {};
+
+	var taskId_ = 0; // The current task's identifier.
+	var projectId_ = null; // The current project's short name.
+	var questionId_ = null; // The current question number.
+	var previousQuestionId_ = null; // The previous question number.
 
 	// This selector is used to determine whether or not an item is clickable.
 	// Please refer to the onElementClicked method for more information.
@@ -60,10 +67,6 @@
 			// While the ReferenceError is expected behavior, not handling it
 			// prevents the rest of the script from being executed.
 		}
-		// With the analytics configured, we can properly start tracking our
-		// events. The "geotagx-analytics-configured" event is fired to notify
-		// the project-specific analytics script that it is OK to begin tracking.
-		$(document).trigger("geotagx-analytics-configured");
 
 		// Are we viewing a category's profile page?
 		var $categoryProfile = $("#category-profile[data-short-name!=][data-short-name]");
@@ -83,6 +86,31 @@
 		$(".share-project").on("click.analytics", onShareProject);
 		$("a[href!=][href]").on("click.analytics", onLinkClicked);
 		$("#signin button").on("click.analytics", onSignInButtonClicked);
+
+		$("#project-task-presenter.analysis .btn-answer").on("click.analytics", onAnswerQuestion);
+
+		$("#project-task-presenter.tutorial #image").on("zoom.analytics", _debounce(onTutorialImageZoom, 350));
+		$("#project-task-presenter.analysis #image").on("zoom.analytics", _debounce(onImageZoom, 350));
+
+		$("#project-task-presenter.tutorial #image-source").on("click.analytics", onShowTutorialImageSource);
+		$("#project-task-presenter.analysis #image-source").on("click.analytics", onShowImageSource);
+
+		$("#project-task-presenter.tutorial #questionnaire-no-photo").on("click.analytics", onNoTutorialImage);
+		$("#project-task-presenter.analysis #questionnaire-no-photo").on("click.analytics", onNoImage);
+
+		$("#project-task-presenter.tutorial #questionnaire-rewind").on("click.analytics", onShowPreviousTutorialQuestion);
+		$("#project-task-presenter.analysis #questionnaire-rewind").on("click.analytics", onShowPreviousQuestion);
+
+		$("#project-task-presenter.tutorial #questionnaire-show-comments").on("click.analytics", onShowTutorialComments);
+		$("#project-task-presenter.analysis #questionnaire-show-comments").on("click.analytics", onShowComments);
+
+		$("#project-task-presenter.tutorial .help-toggle").on("click.analytics", onShowTutorialHelp);
+		$("#project-task-presenter.analysis .help-toggle").on("click.analytics", onShowHelp);
+
+		$("#submit-analysis").on("click.analytics", onSubmitTask);
+
+		$("#skip-tutorial").on("click.analytics", onSkipTutorial);
+		$("#start-contributing").on("click.analytics", onCompleteTutorial);
 
 		//Add surveyModal specific events
 		$("#surveyModal").on("shown.bs.modal", function(){
@@ -225,4 +253,243 @@
 		};
 		analytics.fireEvent("action.shareProject", data);
 	}
-})(jQuery);
+	/**
+	 * Fires an event when a user answers a question during an analysis.
+	 */
+	function onAnswerQuestion(e){
+		// Note that we use the previousQuestionId_ because the onQuestionChanged
+		// function is called before this event handler, effectively changing the
+		// value of questionId_ before we have the chance to read it.
+		// However, previousQuestionId_ holds the value we are looking for.
+		var data = {
+			"projectId":projectId_,
+			"questionId":previousQuestionId_,
+			"taskId":taskId_,
+			"buttonValue":$(e.target).val()
+		};
+		analytics.fireEvent("action.answerQuestion", data);
+	}
+	/**
+	 * Fires an event when a user zooms in on an image during a tutorial.
+	 */
+	function onTutorialImageZoom(){
+		var data = {
+			"projectId":projectId_,
+			"questionId":questionId_
+		};
+		analytics.fireEvent("action.tutorialImageZoom", data);
+	}
+	/**
+	 * Fires an event when a user zooms in on an image during an analysis.
+	 */
+	function onImageZoom(){
+		var data = {
+			"projectId":projectId_,
+			"questionId":questionId_,
+			"taskId":taskId_
+		};
+		analytics.fireEvent("action.imageZoom", data);
+	}
+	/**
+	 * Fires an event when a user visits an image's source during a tutorial.
+	 */
+	function onShowTutorialImageSource(){
+		var data = {
+			"projectId":projectId_,
+			"questionId":questionId_
+		};
+		analytics.fireEvent("action.showTutorialImageSource", data);
+	}
+	/**
+	 * Fires an event when a user visits an image's source during an analysis.
+	 */
+	function onShowImageSource(){
+		var data = {
+			"projectId":projectId_,
+			"questionId":questionId_,
+			"taskId":taskId_
+		};
+		analytics.fireEvent("action.showImageSource", data);
+	}
+	/**
+	 * Fires an event when the user does not see an image during a tutorial.
+	 */
+	function onNoTutorialImage(){
+		var data = {
+			"projectId":projectId_,
+			"imageUrl":$("#image > img").data("src"),
+			"imageSource":$("#image-source").attr("href")
+		};
+		analytics.fireEvent("action.noTutorialImage", data);
+	}
+	/**
+	 * Fires an event when the user does not see an image during an analysis.
+	 */
+	function onNoImage(){
+		var data = {
+			"projectId":projectId_,
+			"imageUrl":$("#image > img").data("src"),
+			"imageSource":$("#image-source").attr("href")
+		};
+		analytics.fireEvent("action.noImage", data);
+	}
+	/**
+	 * Fires an event when a user goes back to a previous question during a tutorial.
+	 */
+	function onShowPreviousTutorialQuestion(){
+		// Note that we use the previousQuestionId_ because the onQuestionChanged
+		// function is called before this event handler, effectively changing the
+		// value of questionId_ before we have the chance to read it.
+		// However, previousQuestionId_ holds the value we are looking for.
+		var data = {
+			"projectId":projectId_,
+			"questionId":previousQuestionId_
+		};
+		analytics.fireEvent("action.showPreviousTutorialQuestion", data);
+	}
+	/**
+	 * Fires an event when a user goes back to a previous question during an analysis.
+	 */
+	function onShowPreviousQuestion(){
+		// Note that we use the previousQuestionId_ because the onQuestionChanged
+		// function is called before this event handler, effectively changing the
+		// value of questionId_ before we have the chance to read it.
+		// However, previousQuestionId_ holds the value we are looking for.
+		var data = {
+			"projectId":projectId_,
+			"questionId":previousQuestionId_,
+			"taskId":taskId_
+		};
+		analytics.fireEvent("action.showPreviousQuestion", data);
+	}
+	/**
+	 * Fires an event when a user clicks the 'Show Comments' button during a tutorial.
+	 */
+	function onShowTutorialComments(){
+		var data = {
+			"projectId":projectId_,
+			"questionId":questionId_
+		};
+		analytics.fireEvent("action.showTutorialComments", data);
+	}
+	/**
+	 * Fires an event when a user clicks the 'Show Comments' button during an analysis.
+	 */
+	function onShowComments(){
+		var data = {
+			"projectId":projectId_,
+			"questionId":questionId_,
+			"taskId":taskId_
+		};
+		analytics.fireEvent("action.showComments", data);
+	}
+	/**
+	 * Fires an event when a user clicks a question's help toggle during a tutorial.
+	 */
+	function onShowTutorialHelp(){
+		var data = {
+			"projectId":projectId_,
+			"questionId":questionId_
+		};
+		analytics.fireEvent("action.showTutorialHelp", data);
+	}
+	/**
+	 * Fires an event when a user clicks a question's help toggle during an analysis.
+	 */
+	function onShowHelp(){
+		var data = {
+			"projectId":projectId_,
+			"questionId":questionId_,
+			"taskId":taskId_
+		};
+		analytics.fireEvent("action.showHelp", data);
+	}
+	/**
+	 * Fires an event when a user submits a task.
+	 */
+	function onSubmitTask(){
+		var data = {
+			"projectId":projectId_,
+			"taskId":taskId_
+		};
+		analytics.fireEvent("action.submitTask", data);
+	}
+	/**
+	 * Fires an event when a user skips a project tutorial.
+	 */
+	function onSkipTutorial(){
+		var data = {
+			"projectId":projectId_
+		};
+		analytics.fireEvent("action.skipTutorial", data);
+	}
+	/**
+	 * Fires an event when a user completes a tutorial and starts contributing to a project.
+	 */
+	function onCompleteTutorial(){
+		var data = {
+			"projectId":projectId_
+		};
+		analytics.fireEvent("action.completeTutorial", data);
+	}
+	/**
+	 * Updates the tracking parameters when a new task is presented to the user.
+	 * @param taskId the task's identifier.
+	 */
+	geotagx.analytics.onTaskChanged = function(taskId){
+		taskId_ = taskId;
+		var data = {
+			"projectId":projectId_,
+			"taskId":taskId_
+		};
+		analytics.fireEvent("action.startTask", data);
+	};
+	/**
+	 * Updates the tracking parameters when a new question is presented to the user.
+	 * @param questionId the current question identifier.
+	 */
+	geotagx.analytics.onQuestionChanged = function(questionId){
+		previousQuestionId_ = questionId_ === null ? questionId : questionId_; // Pay attention to the trailing underscore.
+		questionId_ = questionId;
+	};
+	/**
+	 * Fires an event when a user selects the correct answer in a tutorial.
+	 */
+	geotagx.analytics.onCorrectTutorialAnswer = function(){
+		var data = {
+			"projectId":projectId_,
+			"questionId":questionId_
+		};
+		analytics.fireEvent("action.correctTutorialAnswer", data);
+	};
+	/**
+	 * Fires an event when a user selects the wrong answer in a tutorial.
+	 */
+	geotagx.analytics.onWrongTutorialAnswer = function(){
+		var data = {
+			"projectId":projectId_,
+			"questionId":questionId_
+		};
+		analytics.fireEvent("action.wrongTutorialAnswer", data);
+	};
+	/**
+	 * Fires an event when a user starts a project.
+	 */
+	geotagx.analytics.onStartProject = function(projectId){
+		projectId_ = projectId;
+		var data = {
+			"projectId":projectId_
+		};
+		analytics.fireEvent("action.startProject", data);
+	};
+	/**
+	 * Fires an event when a user starts a project tutorial.
+	 */
+	geotagx.analytics.onStartTutorial = function(projectId){
+		projectId_ = projectId;
+		var data = {
+			"projectId":projectId_
+		};
+		analytics.fireEvent("action.startTutorial", data);
+	};
+})(window.geotagx = window.geotagx || {}, jQuery);
