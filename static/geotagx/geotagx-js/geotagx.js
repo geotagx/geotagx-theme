ba22597eb212b04977e70e2cf121a24486925fd1
Jeremy Othieno <j.othieno@gmail.com>
2016-05-23 17:23:32 +0200

    Remove duplicate code
    Handlers for the 'back-to-top' button can be found in base/page.js.

--- static/geotagx/geotagx-js/geotagx.js
+++ static/geotagx/geotagx-js/geotagx.js
@@ -44,28 +44,4 @@ $(document).ready(function(){
         // Run the show!
         filterList.init();
     });
-
-
-
-
-
-	(function() {
-
-   			$('<i id="back-to-top"></i>').appendTo($('body'));
-
-			$(window).scroll(function() {
-
-				if($(this).scrollTop() != 0) {
-					$('#back-to-top').fadeIn();
-				} else {
-					$('#back-to-top').fadeOut();
-				}
-
-			});
-
-			$('#back-to-top').click(function() {
-				$('body,html').animate({scrollTop:0},600);
-			});
-
-	})();
 });
