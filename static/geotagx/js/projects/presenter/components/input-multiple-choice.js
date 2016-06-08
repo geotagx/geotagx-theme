31c0ba6a15a6d178bfd8f94ebda0f894e53930e6
Jeremy Othieno <j.othieno@gmail.com>
2016-06-08 23:16:13 +0200

    Implement the multiple-choice input component

--- static/geotagx/js/projects/presenter/components/input-multiple-choice.js
+++ static/geotagx/js/projects/presenter/components/input-multiple-choice.js
@@ -25,3 +25,119 @@
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  * OR OTHER DEALINGS IN THE SOFTWARE.
  */
+(function(geotagx){
+    "use strict";
+
+    geotagx.vue.component.MultipleChoiceInput = Vue.extend({
+        template: "#g-input-multiple-choice-template",
+        data: function(){
+            return {
+                selectedOptions: [],
+                otherOptionValue: null,
+            }
+        },
+        props: {
+            /**
+             * The input configuration.
+             */
+            input: {
+                type: Object,
+                required: true,
+            },
+            /**
+             * The current language.
+             */
+            language: {
+                type: String,
+                required: true,
+            },
+        },
+        computed: {
+            /**
+             * The list of selectable options.
+             */
+            options: function(){
+                return this.input.options;
+            },
+            /**
+             * An option's input type can either be a radio or checkbox depending on
+             * whether the multiple-choice input expects a single or multiple answers.
+             */
+            optionInputType: function(){
+                return this.allowMultipleAnswers ? "checkbox" : "radio";
+            },
+            /**
+             * If true, the input can accept more than one answer, otherwise only one answer
+             * is accepted.
+             */
+            allowMultipleAnswers: function(){
+                var result = this.input["allow-multiple-answers"];
+                if (result === undefined || result === null)
+                    result = false;
+
+                return result;
+            },
+            /**
+             * If true, the allowOtherOption parameter enables an option that allows a user
+             * to input an unlisted value.
+             */
+            allowOtherOption: function(){
+                var result = this.input["allow-other-option"];
+                if (result === undefined || result === null)
+                    result = true;
+
+                return result;
+            },
+        },
+        methods: {
+            /**
+             * Resets the input.
+             */
+            reset: function(){
+                this.selectedOptions = [];
+                this.otherOptionValue = null;
+            },
+            /**
+             * Returns true if an option is selected, false otherwise.
+             */
+            isOptionSelected: function(){
+                return this.selectedOptions.length > 0;
+            },
+            /**
+             * Returns true if the 'Other' option is selected, false otherwise.
+             */
+            isOtherOptionSelected: function(){
+                return this.selectedOptions.indexOf("Other") >= 0;
+            },
+            /**
+             * A function that is called when an answer button is pressed.
+             */
+            onAnswer: function(answer){
+                // If the OK button is pressed, the answer may need to be
+                // preprocessed before it is stored.
+                if (answer === "OK"){
+                    answer = this.selectedOptions;
+
+                    // If a specific 'Other' option is provided, it needs to be added to the
+                    // final answer.
+                    if (this.allowOtherOption && this.isOtherOptionSelected()){
+                        var completeOtherOption = "Other";
+                        if (this.otherOptionValue){
+                            this.otherOptionValue = this.otherOptionValue.trim();
+                            if (this.otherOptionValue.length > 0)
+                                completeOtherOption += "[" + this.otherOptionValue + "]";
+                        }
+                        if (this.allowMultipleAnswers){
+                            var i = answer.indexOf("Other");
+                            if (i >= 0)
+                                answer[i] = completeOtherOption;
+                        }
+                        else
+                            answer = completeOtherOption;
+                    }
+                }
+                geotagx.vue.helper.dispatchAnswer(this, answer);
+            }
+        }
+    });
+})(window.geotagx = window.geotagx || {});
